cdcb02485a3d56fd986ba784b7aee935e6e68d39
xidhu <sidhu3612@gmail.com>
2021-01-03 21:25:21 +0530

    change

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ window.addEventListener("resize",(event)=>{
     let isMobile =  window.innerWidth < 700 || window.innerHeight < 700?true:false;
 console.log(isMobile);
 if(isMobile){
-    document.body.innerHTML = "Mobile Version Not Implemented.";
+    document.body.innerHTML = window.innerWidth +"  "+window.innerHeight+"  "+"Mobile Version Not Implemented.";
 }else{
     location.reload();
 }
