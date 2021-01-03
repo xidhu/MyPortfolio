c6013dba952f10ff932b4fd629ab98429928ff69
xidhu <sidhu3612@gmail.com>
2021-01-03 21:33:26 +0530

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ if(isMobile){
     document.body.innerHTML = "Mobile Version Not Implemented.";
 }
 window.addEventListener("resize",(event)=>{
-    let isMobile =  window.innerWidth < 500 && window.innerHeight < 300?true:false;
+    isMobile =  window.innerWidth < 500 && window.innerHeight < 300?true:false;
 console.log(isMobile);
 if(isMobile){
     document.body.innerHTML = "Mobile Version Not Implemented";
