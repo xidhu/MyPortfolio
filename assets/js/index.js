ceb82a5ca280416aa0448b78ff93c79ea747935f
xidhu <sidhu3612@gmail.com>
2021-01-04 12:50:28 +0530

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
-let isMobile =  (window.innerWidth < 500 && window.innerHeight < 300)||window.innerHeight > window.innerWidth?true:false;
+let isMobile =  window.innerWidth < window.innerHeight?true:false;
 console.log(isMobile);
 if(isMobile){
     document.body.innerHTML = "Mobile Version Not Implemented.";
 }
 window.addEventListener("resize",(event)=>{
-    isMobile =  (window.innerWidth < 500 && window.innerHeight < 300) ||window.innerHeight > window.innerWidth?true:false;
+    isMobile =  window.innerWidth < window.innerHeight?true:false;
 console.log(isMobile);
 if(isMobile){
-    document.body.innerHTML = "Mobile Version Not Implemented";
+    document.body.innerHTML = "Mobile Version Not Implemented.";
 }else{
     location.reload();
 }
